b0ea7ff9e0200fdc2563b0a484d8da012e86d733
Kona-Abhishek <65806709+Kona-Abhishek@users.noreply.github.com>
2024-04-11 20:36:41 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,4 @@ set -e
 # Stop the running container (if any)
 echo "Hi"
 containerid=$(docker ps --format '{{.ID}}')
-if [ -n "$containerid" ]; then
-    echo "Inside Loop"
-    docker rm -f $containerid
-fi
+
