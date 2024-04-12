143a603b7bcddae98d6f0682f7299ab4f49102c7
Kona-Abhishek <65806709+Kona-Abhishek@users.noreply.github.com>
2024-04-12 23:03:15 +0530

    updated

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull abhishekkona/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 9000:9000 abhishekkona/simple-python-flask-app
